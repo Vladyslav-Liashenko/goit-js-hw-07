bda4d9422e5aa5be183323570c7c8bd98d41d3b4
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2023-09-06 16:08:22 +0300

    HW 7 2 part

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector(".gallery");
+const galleryElement = [];
+
+galleryItems.forEach(item => {
+  const galleryItemElement = document.createElement('li');
+  galleryItemElement.className = 'gallery__item';
+  const link = document.createElement('a');
+  link.className = 'gallery__link';
+  link.setAttribute("href", item.original);
+  const imageElement = document.createElement('img');
+  imageElement.src = item.preview;
+  imageElement.alt = item.description;
+  imageElement.setAttribute("data-src", item.original);
+  imageElement.className = 'gallery__image';
+  galleryItemElement.appendChild(link);
+  link.appendChild(imageElement);
+  galleryElement.push(galleryItemElement);
+});
+  galleryContainer.append(...galleryElement);
+
+let gallery = new SimpleLightbox('.gallery a', { captionsData: "alt" });
+gallery.on('show.simplelightbox', function () {
+    setTimeout(function () {
+    },250);
+});
+
+gallery.on('error.simplelightbox', function (e) {
+	console.log(e);
+});
+
 console.log(galleryItems);
